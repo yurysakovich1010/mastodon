5c7c1c8720373c6ae02e7968dd72455f586fd650
yurysakovich1010 <yurasakovich29@gmail.com>
2020-06-19 17:15:43 +0300

    fork brighteon social layout from public layout
    (cherry picked from commit 4efed3daa79e3bc5e000d1a2df8c51a0e6d00343)

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AboutController < ApplicationController
-  layout 'public'
+  layout 'brighteon_social'
 
   before_action :require_open_federation!, only: [:show, :more]
   before_action :set_body_classes, only: :show
