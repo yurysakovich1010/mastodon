357e81fe8f930e8104d6714ff758c11cd4418936
yurysakovich1010 <yurasakovich29@gmail.com>
2020-06-26 18:12:08 +0300

    update shop link
    (cherry picked from commit 6d53a318454b1ffcbb85c426c95055d69dd51408)

--- app/javascript/mastodon/features/ui/components/navigation_panel.js
+++ app/javascript/mastodon/features/ui/components/navigation_panel.js
@@ -33,7 +33,7 @@ const NavigationPanel = () => (
       Explore
     </div>
     <NavLink className='column-link column-link--transparent' to='/trends' target='_blank' rel='noopener noreferrer'><Icon className='column-link__icon' id='bar-chart' fixedWidth /><FormattedMessage id='navigation_bar.trends' defaultMessage='Trends' /></NavLink>
-    <a className='column-link column-link--transparent' href='http://brighteon.store' target='_blank' rel='noopener noreferrer'><Icon className='column-link__icon' id='tags' fixedWidth /><FormattedMessage id='navigation_bar.shop' defaultMessage='Shop' /></a>
+    <a className='column-link column-link--transparent' href='http://www.brighteonstore.com' target='_blank' rel='noopener noreferrer'><Icon className='column-link__icon' id='tags' fixedWidth /><FormattedMessage id='navigation_bar.shop' defaultMessage='Shop' /></a>
     <a className='column-link column-link--transparent' href='https://www.brighteon.com' target='_blank' rel='noopener noreferrer'><Icon className='column-link__icon' id='rocket' fixedWidth /><FormattedMessage id='navigation_bar.brighteon_company' defaultMessage='Brighteon.com' /></a>
     <a className='column-link column-link--transparent' href='http://naturalNews.com' target='_blank' rel='noopener noreferrer'><Icon className='column-link__icon' id='rocket' fixedWidth /><FormattedMessage id='navigation_bar.natural_news_company' defaultMessage='NaturalNews.com' /></a>
     <a className='column-link column-link--transparent' href='https://newstarget.com' target='_blank' rel='noopener noreferrer'><Icon className='column-link__icon' id='rocket' fixedWidth /><FormattedMessage id='navigation_bar.newstarget' defaultMessage='Newstarget.com' /></a>
