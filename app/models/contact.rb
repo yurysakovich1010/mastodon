df57b2022aaf29c37b8aa55b87bcb791fa0f0ca2
yurysakovich1010 <yurasakovich29@gmail.com>
2020-06-26 17:21:42 +0300

    update contact model
    (cherry picked from commit 9a625c94307109f1524f30cc21461ecc769c3d7f)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id         :bigint(8)        not null, primary key
+#  feedback   :string           default(""), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Contact < ApplicationRecord
 end
