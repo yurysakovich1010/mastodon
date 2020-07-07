51c63603764dff996d77da612dbc63bce8e93fc6
yurysakovich1010 <yurasakovich29@gmail.com>
2020-07-07 18:39:32 +0300

    tutorials re-design
    (cherry picked from commit b736ae16068f0f75fcb29ff59ca320f22af4332b)

--- app/javascript/mastodon/features/introduction/index.js
+++ app/javascript/mastodon/features/introduction/index.js
@@ -5,20 +5,16 @@ import classNames from 'classnames';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import { closeOnboarding } from '../../actions/onboarding';
-import screenHello from '../../../images/screen_hello.svg';
-import screenFederation from '../../../images/screen_federation.svg';
-import screenInteractions from '../../../images/screen_interactions.svg';
-import logoTransparent from '../../../images/logo_transparent.svg';
+import intro1 from '../../../images/brighteon-social/intro_1.png';
+import intro2 from '../../../images/brighteon-social/intro_2.png';
+import intro3 from '../../../images/brighteon-social/intro_3.png';
 
 const FrameWelcome = ({ domain, onNext }) => (
   <div className='introduction__frame'>
-    <div className='introduction__illustration' style={{ background: `url(${logoTransparent}) no-repeat center center / auto 80%` }}>
-      <img src={screenHello} alt='' />
-    </div>
-
+    <div className='introduction__illustration' style={{ background: `#000a3d url(${intro1}) no-repeat center bottom / auto 90%` }} />
     <div className='introduction__text introduction__text--centered'>
       <h3><FormattedMessage id='introduction.welcome.headline' defaultMessage='First steps' /></h3>
-      <p><FormattedMessage id='introduction.welcome.text' defaultMessage="Welcome to the fediverse! In a few moments, you'll be able to broadcast messages and talk to your friends across a wide variety of servers. But this server, {domain}, is special—it hosts your profile, so remember its name." values={{ domain: <code>{domain}</code> }} /></p>
+      <p><FormattedMessage id='introduction.welcome.text' defaultMessage="Welcome to Brighteon.Social! In a few moments, you'll be able to broadcast messages and talk to your friends across our {domain} Network." values={{ domain: <code>{domain}</code> }} /></p>
     </div>
 
     <div className='introduction__action'>
@@ -34,24 +30,22 @@ FrameWelcome.propTypes = {
 
 const FrameFederation = ({ onNext }) => (
   <div className='introduction__frame'>
-    <div className='introduction__illustration'>
-      <img src={screenFederation} alt='' />
-    </div>
+    <div className='introduction__illustration' style={{ background: `#000a3d url(${intro2}) no-repeat center center / auto 90%` }} />
 
     <div className='introduction__text introduction__text--columnized'>
       <div>
         <h3><FormattedMessage id='introduction.federation.home.headline' defaultMessage='Home' /></h3>
-        <p><FormattedMessage id='introduction.federation.home.text' defaultMessage='Posts from people you follow will appear in your home feed. You can follow anyone on any server!' /></p>
+        <p><FormattedMessage id='introduction.federation.home.text' defaultMessage='Posts from people you follow will appear in your home feed. You can follow anyone on our network.' /></p>
       </div>
 
       <div>
-        <h3><FormattedMessage id='introduction.federation.local.headline' defaultMessage='Local' /></h3>
-        <p><FormattedMessage id='introduction.federation.local.text' defaultMessage='Public posts from people on the same server as you will appear in the local timeline.' /></p>
+        <h3><FormattedMessage id='introduction.federation.trending.headline' defaultMessage='Trends' /></h3>
+        <p><FormattedMessage id='introduction.federation.trending.text' defaultMessage='The most popular public posts from people who have joined Brighteon.Social' /></p>
       </div>
 
       <div>
-        <h3><FormattedMessage id='introduction.federation.federated.headline' defaultMessage='Federated' /></h3>
-        <p><FormattedMessage id='introduction.federation.federated.text' defaultMessage='Public posts from other servers of the fediverse will appear in the federated timeline.' /></p>
+        <h3><FormattedMessage id='introduction.federation.trending_now.headline' defaultMessage='Trending Now' /></h3>
+        <p><FormattedMessage id='introduction.federation.trending_now.text' defaultMessage='See all the censored news headlines in real time.' /></p>
       </div>
     </div>
 
@@ -67,24 +61,22 @@ FrameFederation.propTypes = {
 
 const FrameInteractions = ({ onNext }) => (
   <div className='introduction__frame'>
-    <div className='introduction__illustration'>
-      <img src={screenInteractions} alt='' />
-    </div>
+    <div className='introduction__illustration' style={{ background: `#000a3d url(${intro3}) no-repeat center bottom / auto 95%` }} />
 
     <div className='introduction__text introduction__text--columnized'>
       <div>
         <h3><FormattedMessage id='introduction.interactions.reply.headline' defaultMessage='Reply' /></h3>
-        <p><FormattedMessage id='introduction.interactions.reply.text' defaultMessage="You can reply to other people's and your own toots, which will chain them together in a conversation." /></p>
+        <p><FormattedMessage id='introduction.interactions.reply.text' defaultMessage="You can reply to other people's and your own posts, which will chain them together in a conversation." /></p>
       </div>
 
       <div>
         <h3><FormattedMessage id='introduction.interactions.reblog.headline' defaultMessage='Boost' /></h3>
-        <p><FormattedMessage id='introduction.interactions.reblog.text' defaultMessage="You can share other people's toots with your followers by boosting them." /></p>
+        <p><FormattedMessage id='introduction.interactions.reblog.text' defaultMessage="You can share other people's posts with your followers by boosting them." /></p>
       </div>
 
       <div>
         <h3><FormattedMessage id='introduction.interactions.favourite.headline' defaultMessage='Favourite' /></h3>
-        <p><FormattedMessage id='introduction.interactions.favourite.text' defaultMessage='You can save a toot for later, and let the author know that you liked it, by favouriting it.' /></p>
+        <p><FormattedMessage id='introduction.interactions.favourite.text' defaultMessage='You can save a post for later, and let the author know that you liked it, by saving it as a favorite.' /></p>
       </div>
     </div>
 
