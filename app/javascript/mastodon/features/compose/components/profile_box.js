3898ab600b1298bc1640ce3b48179b6f3e6e8acb
yurysakovich1010 <yurasakovich29@gmail.com>
2020-07-23 02:54:43 +0300

    fix profile box
    (cherry picked from commit 7c30435c76ecefbcaf127aa76d81f21199c22bdf)

--- app/javascript/mastodon/features/compose/components/profile_box.js
+++ app/javascript/mastodon/features/compose/components/profile_box.js
@@ -31,7 +31,9 @@ class ProfileBox extends ImmutablePureComponent {
     return (
       <div className='profile_box'>
         <div className='profile_box-header'>
-          <img src={this.props.account.get('header_static')} />
+          <div className='profile_box-header__image'>
+            <img src={this.props.account.get('header_static')} />
+          </div>
           <a href='/settings/profile' target='_blank' rel='noopener noreferrer'>
             <button className='button button-edit-profile'>
               <FormattedMessage id='account.edit_profile' defaultMessage='Edit profile' />
