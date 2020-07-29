ad9344b5872ef7317c3c1fcb4e15d72f8fc9c28a
yurysakovich1010 <yurasakovich29@gmail.com>
2020-07-29 12:51:56 +0300

    404 page
    (cherry picked from commit fec7d75ec76e2281d39c688144c0eeecde54da0b)

--- app/javascript/mastodon/components/missing_indicator.js
+++ app/javascript/mastodon/components/missing_indicator.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import illustration from 'mastodon/../images/elephant_ui_disappointed.svg';
+import illustration from 'mastodon/../images/brighteon-social/dog.png';
 import classNames from 'classnames';
 
 const MissingIndicator = ({ fullPage }) => (
   <div className={classNames('regeneration-indicator', { 'regeneration-indicator--without-header': fullPage })}>
-    <div className='regeneration-indicator__figure'>
-      <img src={illustration} alt='' />
-    </div>
-
     <div className='regeneration-indicator__label'>
       <FormattedMessage id='missing_indicator.label' tagName='strong' defaultMessage='Not found' />
       <FormattedMessage id='missing_indicator.sublabel' defaultMessage='This resource could not be found' />
+    </div>
+
+    <div className='regeneration-indicator__figure'>
+      <img src={illustration} alt='' />
     </div>
   </div>
 );
