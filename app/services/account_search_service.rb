1133303361ab014bef6ffb2d97b9582a4bbf5b2f
yurysakovich1010 <yurasakovich29@gmail.com>
2020-11-20 19:01:43 +0100

    hotfix - account search only locally

--- app/services/account_search_service.rb
+++ app/services/account_search_service.rb
@@ -28,13 +28,14 @@ class AccountSearchService < BaseService
     return @exact_match if defined?(@exact_match)
 
     match = begin
-      if options[:resolve]
-        ResolveAccountService.new.call(query)
-      elsif domain_is_local?
-        Account.find_local(query_username)
-      else
-        Account.find_remote(query_username, query_domain)
-      end
+      # if options[:resolve]
+      #   ResolveAccountService.new.call(query)
+      # elsif domain_is_local?
+      #   Account.find_local(query_username)
+      # else
+      #   Account.find_remote(query_username, query_domain)
+      # end
+      Account.find_local(query_username)
     end
 
     match = nil if !match.nil? && !account.nil? && options[:following] && !account.following?(match)
