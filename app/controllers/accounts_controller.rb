56d17a320641d4ded062d919eebdaadc349d64d0
yurysakovich1010 <yurasakovich29@gmail.com>
2020-12-18 16:13:27 +0100

    reorder rss posts by id:desc - reported by Ana

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -45,7 +45,7 @@ class AccountsController < ApplicationController
         expires_in 1.minute, public: true
 
         limit     = params[:limit].present? ? [params[:limit].to_i, PAGE_SIZE_MAX].min : PAGE_SIZE
-        @statuses = filtered_statuses.without_reblogs.limit(limit)
+        @statuses = filtered_statuses.without_reblogs.reorder(id: :desc).limit(limit)
         @statuses = cache_collection(@statuses, Status)
         render xml: RSS::AccountSerializer.render(@account, @statuses, params[:tag])
       end
